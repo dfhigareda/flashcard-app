c42695f3c9221c694dfaf17feb6a196d83a18c95
dfhigareda <111629844+dfhigareda@users.noreply.github.com>
2022-09-22 19:20:51 -0400

    Add files via upload

---
+++ src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.flashcardapp
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("CutPasteId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        flashcardQuestion.setOnClickListener {
+            flashcardQuestion.visibility = View.INVISIBLE
+            flashcardAnswer.visibility = View.VISIBLE
+        }
+    }
+}
